b841d9bd7faddcffe26e77be08261c997ed891fb
Neure6676 <1120708221@qq.com>
2024-03-01 23:09:54 -0500

    Mar01 2024 Eureka and Gateway

---
+++ gateway/src/main/java/org/threeam/GatewayApplication.java
@@ -0,0 +1,28 @@
+package org.threeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator inchemiRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/inchemi/account/**")
+                        .filters( f -> f.rewritePath("/inchemi/account/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("InChemi-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNT")).build();
+    }
+
+}
